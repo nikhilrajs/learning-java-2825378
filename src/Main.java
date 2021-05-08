0774ee5eb313e8605ae422b49995897e55661727
Nikhil Raj <1384007+nikhilrajs@users.noreply.github.com>
2021-05-08 13:48:35 +0530

    Java - Data types

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
-
-    public static void main(String[] args) {
-        
-    }
+	public static void main(String[] args) {
+		int studentAge = 15;
+		double studentGPA = 3.45;
+		char studentFirstInitial = 'K';
+		char studentLastInitial = 'H';
+		boolean hasPerfectAttendance = true;
+		System.out.println("Student Age: " + studentAge);
+		System.out.println("Student GPA: " + studentGPA);
+		System.out.println("Student First Initial: " + studentFirstInitial);
+		System.out.println("Student Last Initial: " + studentLastInitial);
+		System.out.println("Full Attendance: " + hasPerfectAttendance);
+	}
 }
